feb2aa49490233bf771bad67034938df3f10d213
solo8116 <gr3y.ma7t3r@gmail.com>
2024-08-25 03:48:29 +0530

    fix: holiday script

--- src/scripts/holiday.js
+++ src/scripts/holiday.js
@@ -8,13 +8,9 @@ const main = async () => {
   });
 
   const html = parse(holidayData);
-  const tables = html
-    .querySelector(
-      '.fusion-column-wrapper.fusion-column-has-shadow.fusion-flex-justify-content-flex-start.fusion-content-layout-column',
-    )
-    .querySelectorAll('.table-1');
-  const table1 = tables[0].innerHTML;
-  const table2 = tables[1].innerHTML;
+  const tables = html.querySelectorAll('.table-1');
+  const table1 = tables[2].innerHTML;
+  const table2 = tables[3].innerHTML;
   const jsonTables1 = HtmlTableToJson.parse(table1);
   const jsonTables2 = HtmlTableToJson.parse(table2);
   const data = [...jsonTables1.results[0], ...jsonTables2.results[0]];
